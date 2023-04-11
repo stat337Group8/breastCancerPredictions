d3bda932eb4ffac657040c96fffd2fe15dce6c27
Rohan Sethi <rsethi1@luc.edu>
2023-04-11 18:18:58 -0500

    updated convert scripts

--- scripts/convert.py
+++ scripts/convert.py
@@ -4,13 +4,17 @@ import argparse
 parser = argparse.ArgumentParser(description="convert excel to csv")
 parser.add_argument('-i', '--input', help='input excel file path', required=True)
 parser.add_argument('-o', '--output', help='output csv file path', required=False, default='./output.csv')
+parser.add_argument('-c', '--columnHeader', help='input the start of the actual data without header', required=False, default=0)
 
-def convert(inputFilePath, outputFilePath):
+def convert(inputFilePath, outputFilePath, header):
     
-    df = pd.read_excel(inputFilePath)
+    if header == 0:
+        df = pd.read_excel(inputFilePath)
+    else:
+        df = pd.read_excel(inputFilePath, header=[i for i in range(header)])
 
     df.to_csv(outputFilePath, header=True, index=None)
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    convert(args.input, args.output)
+    convert(args.input, args.output, int(args.columnHeader))
