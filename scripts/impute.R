392c79112934be465afde06ac8836be3aff1149f
@rsethi <70125166+rsethi21@users.noreply.github.com>
2023-04-24 03:44:16 -0500

    Create impute.R

---
+++ scripts/impute.R
@@ -0,0 +1,21 @@
+# load necessary libraries
+#install.packages("optparse")
+library(optparse)
+
+
+# insert command line arguments
+options <- list(make_option(c("-i", "--input"), help="csv file path space delimited"), make_option(c("-c", "--column"), help="list of column names"), make_option(c("-o", "--output"), help="csv file path"))
+parser <- OptionParser(option_list=options)
+args <- parse_args(parser, positional_arguments=TRUE)
+
+
+# load data frame
+df <- read.csv(args$input)
+
+# impute data
+for (name in list(strsplit(args$column, " ")) {
+    df[,name][is.na(df[,name])] <- mean(df[,name], na.rn=T)
+}
+
+# save new data frame
+write.csv(df, args$output)
