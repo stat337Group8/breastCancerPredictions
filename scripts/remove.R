54c7a6d1e8a91b01a256bcf264cd59f3ca5efc87
@rsethi <70125166+rsethi21@users.noreply.github.com>
2023-04-24 01:53:38 -0500

    Update remove.R

--- scripts/remove.R
+++ scripts/remove.R
@@ -5,7 +5,10 @@ args <- commandArgs(trailingOnly=TRUE)
 imageData <- read.csv(args[1])
 
 #find which rows have na
-rowsWithNA <- imageData[rowSums(is.na(imageData)) > 0,]
+rowsWithoutNA <- imageData[rowSums(is.na(imageData)) == 0,]
 
 #see which columns have na
-columnsWithNA <- names(which(colSums(is.na(imageData))>0))
+# columnsWithNA <- names(which(colSums(is.na(imageData))>0))
+
+# save new DF
+write.csv(rowsWithoutNA, args.output)
