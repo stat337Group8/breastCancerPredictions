d50d5c9705becad85f075d65cadce6b34760e0bf
Rohan Sethi <rsethi1@luc.edu>
2023-04-24 17:36:21 -0500

    fixed data and added scripts to do so

--- scripts/impute.R
+++ scripts/impute.R
@@ -3,23 +3,27 @@ install.packages("missForest")
 library("missForest")
 
 # organize arguments
-args <- c('./data/reports.csv', TRUE, './data/reports2.csv')
+args <- c('./data/imageFixed.csv', FALSE, './data/imageFixed.csv')
 
 # load data frame
 df <- read.csv(args[1])
 columns <- names(df)
 
+# remove problematic columns (categorical variables that are not imputable)
 if (args[2] == TRUE) {
-  removeCols <- c('mamAge', 'mamBDensity', 'mamShape', 'mamMargin', 'mamDistortion', 'mamLDensity', 'mamCalcifications', 'mamSize', 'usShape', 'usMargin', 'usSize', 'usEchogenicity', 'usSolid',  'usPAS', 'recurrence')
+  removeCols <- c('patient', 'mamAge', 'mamBDensity', 'mamShape', 'mamMargin', 'mamDistortion', 'mamLDensity', 'mamCalcifications', 'mamSize', 'usShape', 'usMargin', 'usSize', 'usEchogenicity', 'usSolid',  'usPAS')
   columns <- columns[!columns %in% c(removeCols)]
+  patients <- df[,'patient']
 } else {
-  columns <- columns
+  removeCols <- c('Patient.ID', 'X.1', 'X')
+  columns <- columns[!columns %in% c(removeCols)]
+  patients <- df[,'Patient.ID']
 }
 
-# impute data
-
-imputed_missForest = missForest(df[,columns][,-1])
-forestImp = imputed_missForest$ximp
+# impute data using random forest
+imputed_missForest <- missForest(df[,columns])
+df[,columns] <- imputed_missForest$ximp
+df <- cbind(patients, df[,columns])
 
 # save new data frame
 write.csv(df, args[3])
