114ac3db9bae8cf2b613a2afb1bcacc4aaaeb014
Rohan Sethi <rsethi1@luc.edu>
2023-04-24 15:56:35 -0500

    imputed

--- scripts/impute.R
+++ scripts/impute.R
@@ -1,21 +1,25 @@
-# load necessary libraries
-#install.packages("optparse")
-library(optparse)
+# install packages
+install.packages("missForest")
+library("missForest")
 
-
-# insert command line arguments
-options <- list(make_option(c("-i", "--input"), help="csv file path space delimited"), make_option(c("-c", "--column"), help="list of column names"), make_option(c("-o", "--output"), help="csv file path"))
-parser <- OptionParser(option_list=options)
-args <- parse_args(parser, positional_arguments=TRUE)
-
+# organize arguments
+args <- c('./data/reports.csv', TRUE, './data/reports2.csv')
 
 # load data frame
-df <- read.csv(args$input)
+df <- read.csv(args[1])
+columns <- names(df)
 
-# impute data
-for (name in list(strsplit(args$column, " "))) {
-    df[,name][is.na(df[,name])] <- mean(df[,name], na.rm=T)
+if (args[2] == TRUE) {
+  removeCols <- c('mamAge', 'mamBDensity', 'mamShape', 'mamMargin', 'mamDistortion', 'mamLDensity', 'mamCalcifications', 'mamSize', 'usShape', 'usMargin', 'usSize', 'usEchogenicity', 'usSolid',  'usPAS', 'recurrence')
+  columns <- columns[!columns %in% c(removeCols)]
+} else {
+  columns <- columns
 }
 
+# impute data
+
+imputed_missForest = missForest(df[,columns][,-1])
+forestImp = imputed_missForest$ximp
+
 # save new data frame
-write.csv(df, args$output)
+write.csv(df, args[3])
