69a6275d891720abbef03fd1aadedf844c76b527
@rsethi <70125166+rsethi21@users.noreply.github.com>
2023-04-24 04:17:03 -0500

    Update remove.R

--- scripts/remove.R
+++ scripts/remove.R
@@ -4,7 +4,7 @@ library(optparse)
 
 
 # insert command line arguments
-options <- list(make_option(c("-i", "--input"), help="csv file path space delimited"), make_option(c("-o", "--output"), help="csv file path"))
+options <- list(make_option(c("-i", "--input"), help="csv file path space delimited"), make_option(c("-o", "--output"), help="csv file path"), make_option(c("-s", "--saveIndices"), help="save path for indices removed"), make_option(c("-n", "--name"), help="name of index"))
 parser <- OptionParser(option_list=options)
 args <- parse_args(parser, positional_arguments=TRUE)
 
@@ -17,3 +17,6 @@ rowsWithoutNA <- imageData[rowSums(is.na(imageData)) == 0,]
 
 # save new DF
 write.csv(rowsWithoutNA, args$output)
+
+# save ids removed
+write.csv(imageData[rowSums(is.na(imageData)) > 0,args$name], args$saveIndices)
