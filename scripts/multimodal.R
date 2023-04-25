172716b4ce239533f5e43efb33aa2eae493de890
Rohan Sethi <rsethi1@luc.edu>
2023-04-25 13:47:52 -0500

    updated script with AIC

--- scripts/multimodal.R
+++ scripts/multimodal.R
@@ -7,7 +7,7 @@ library("PRROC")
 df <- read.csv('./data/comboFixed.csv')
 dfy <- df$recurrence
 df <- subset(df, select=-c(X, patients, recurrence))
-set.seed(2023)
+set.seed(2022)
 
 # imbalanced outcome value
 table(dfy)
@@ -24,11 +24,11 @@ plot(PVE, xlab='Principle Component', ylab='Proportion of Variance Explained', c
 abline(a=PVE[10], b=0)
 text(x=10, y=0.04)
 
-dfZ <- data.frame(Z[,1:70]) # find how to do elbow
+dfZ <- data.frame(Z[,1:20]) # find how to do elbow
 dfZ$recurrence <- dfy
 
 # test-train split set-up
-index.train <- sample(1:dim(dfZ)[1], 0.8*dim(dfZ)[1])
+index.train <- sample(1:dim(dfZ)[1], 0.75*dim(dfZ)[1])
 data.train <- dfZ[index.train,]
 data.test <- dfZ[-index.train,]
 
@@ -44,21 +44,6 @@ acc.test <- mean(y.predict==y.test)
 acc.test
 
 # cross validation accuracy score
-
-
-# backward select using AIC
-step(logistic.model, method='backward')
-
-selected.model <- glm(formula = recurrence ~ PC1 + PC3 + PC4 + PC7 + PC10 + PC11 + 
-                        PC12 + PC14 + PC15 + PC21 + PC22 + PC27 + PC28 + PC32 + PC51 + 
-                        PC55 + PC56 + PC62 + PC64, family = "binomial", data = data.train)
-
-selected.p.predict <- predict(selected.model, data.test, type='response')
-selected.y.predict <- ifelse(selected.p.predict>=0.5, 1, 0) # find a way to select for the best threshold
-y.test <- data.test$recurrence
-table(selected.y.predict, y.test)
-selected.acc.test <- mean(selected.y.predict==y.test)
-selected.acc.test
 
 # metrics like accuracy and ROC/AUROC
 plot(roc.curve(scores.class0 = p.predict[y.test==1], 
@@ -84,3 +69,20 @@ TNR
 # precision
 prec <- length(TP) / (length(TP) + length(FP))
 prec
+
+
+
+# backward select using AIC
+step(logistic.model, method='backward')
+
+selected.model <- glm(formula = recurrence ~ PC1 + PC3 + PC4 + PC7 + PC10 + PC11 + 
+                        PC12 + PC14 + PC15 + PC21 + PC22 + PC27 + PC28 + PC32 + PC51 + 
+                        PC55 + PC56 + PC62 + PC64, family = "binomial", data = data.train)
+
+selected.p.predict <- predict(selected.model, data.test, type='response')
+selected.y.predict <- ifelse(selected.p.predict>=0.5, 1, 0) # find a way to select for the best threshold
+y.test <- data.test$recurrence
+table(selected.y.predict, y.test)
+selected.acc.test <- mean(selected.y.predict==y.test)
+selected.acc.test
+
