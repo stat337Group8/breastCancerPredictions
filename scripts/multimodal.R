000c1de8e593acad93ce8b93d33380fb7da55d72
Rohan Sethi <rsethi1@luc.edu>
2023-04-25 00:05:07 -0500

    created combo csv and multimodal model script

--- scripts/multimodal.R
+++ scripts/multimodal.R
@@ -1,35 +1,43 @@
 # load packages
-# install.packages("optparse")
-library(optparse)
+install.packages("PRROC")
+library("PRROC")
 
-# create options
-options <- list(make_option(c("-i", "--input"), help="list of csvs to combine, space delimited"), make_option(c("-c", "--column"), help="column to merge by; needed if multiple data frames", default=NA))
-parser <- OptionParser(option_list=options)
-args <- parse_args(parser, positional_arguments=TRUE)
+# open dataset
+df <- read.csv('./data/comboFixed.csv')
+dfy <- df$recurrence
+df <- subset(df, select=-c(X, patients, recurrence))
+set.seed(2022)
 
-# prepare data
-df <- NA
-df.list <- list()
+# reduce data dimensionality
+# Perform PCA 
+pr.out <- prcomp(df , scale = TRUE)
+phi <- pr.out$rotation # save for next predicting dataset so that predictions are appropriately handled
+Z <- pr.out$x
+# Evaluate PCA
+PVE.matrix <- summary(pr.out)$importance
+PVE <- PVE.matrix[2,]
+plot(PVE, xlab='Principle Component', ylab='Proportion of Variance Explained', cex.lab=1.5)
+abline(a=PVE[10], b=0)
+text(x=10, y=0.04)
 
-if (length(list(strsplit(args$input), " ")) == 1) {
-    df <- read.csv(args$input)
-} else {
-for (file in list(strsplit(args$input), " ")) {
-    append(df.list, read.csv(file))
-}
-df <- df.list[1]
-i <- 2
-while (i < length(df.list)) {
-    df <- merge(df, df[i], by=args$column)
-    i <- i+1
-}
-}
+dfZ <- data.frame(Z[,1:15]) # find how to do elbow
+dfZ$recurrence <- dfy
 
-# create model
+# test-train split set-up
+index.train <- sample(1:dim(dfZ)[1], 0.8*dim(dfZ)[1])
+data.train <- dfZ[index.train,]
+data.test <- dfZ[-index.train,]
 
+# train model
+logistic.model <- glm(recurrence~., data=data.train, family='binomial')
 
-# train model, crossfold validate
-
+# evaluate model
+y.predict <- predict(logistic.model, data.test, type='response')
+y.predict <- ifelse(y.predict>=0.5, 1, 0) # find a way to select for the best threshold
+y.test <- data.test$recurrence
+table(y.predict, y.test)
+acc.test <- mean(y.predict==y.test)
+acc.test
 
 # backward select using AIC
 
