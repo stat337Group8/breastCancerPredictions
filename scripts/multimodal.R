8c6c902ca415ec254b235e203dbba5ec889a31f3
Rohan Sethi <rsethi1@luc.edu>
2023-04-27 11:45:31 -0500

    updated scripts for finalized results run

--- scripts/multimodal.R
+++ scripts/multimodal.R
@@ -1,3 +1,4 @@
+######################################################
 # load packages
 install.packages("comprehenr")
 install.packages("ROSE")
@@ -9,8 +10,9 @@ library("ggplot2")
 # set working directory
 # setwd("PATH TO FOLDER")
 
+###################################################
 # helper functions
-# cross validation accuracy score
+## cross validation accuracy score
 crossVal <- function(data, threshold){
   # randomly shuffle the index
   
@@ -30,16 +32,27 @@ crossVal <- function(data, threshold){
     test <- data[index.test,]
     train <- data[-index.test,]
     
-    # fit a linear model on the training set
-    model <- glm(recurrence ~ ., family = "binomial", data = train)
+    library("ROSE")
+    # data.train <- ROSE(recurrence ~ ., data = data.train, seed=2022)$data
+    data.train <- ROSE(recurrence ~ PC2 + PC3 + PC4 + PC5 + PC9 + PC10 + 
+                         PC12 + PC14 + PC16 + PC17 + PC18 + PC19, data.train, seed=2022)$data
+    # weights
+    w0 <- length(data.train$recurrence)/(length(data.train$recurrence[data.train$recurrence==0]))
+    w1 <- length(data.train$recurrence)/(length(data.train$recurrence[data.train$recurrence==1]))
+    ws <- to_vec(for(r in data.train$recurrence) if (r == 0) w0 else w1)
     
+    # fit a linear model on the training set
+    # model <- glm(recurrence ~ ., family = "binomial", data = train, weights=ws)
+    model <- glm(formula = recurrence ~ PC3 + PC5 + PC9 + PC10 + 
+                   PC12 + PC14 + PC16, family = "binomial", 
+                 data = data.train, weights = ws)
     # predict on the test set
     prob <- predict(model, test, type='response')
     pred <- ifelse(prob>=threshold, 1, 0) # find a way to select for the best threshold
     truth <- test$recurrence
     accuracy <- mean(pred==truth)
     aCCs <- c(aCCs, accuracy)
-    table(pred, truth)
+    print(table(pred, truth))
   }
   # plot 5 Accuracies
   
@@ -71,12 +84,21 @@ accuracies <- function(prediction, truth){
   prec <- length(TP) / (length(TP) + length(FP))
   ## f1score
   f1 <- length(TP)/(length(TP)+0.5*(length(FP)+length(FN)))
-
+  print("Accuracy:: ")
+  print(accuracy)
+  print("Precision:: ")
+  print(prec)
+  print("TPR:: ")
+  print(TPR)
+  print("TNR:: ")
+  print(TNR)
+  print("F1:: ")
+  print(f1)
   return(c(accuracy, prec, TPR, TNR, f1))
 }
 
 
-
+############################################ 
 # open dataset
 dforig <- read.csv('./data/comboFixed.csv')
 dfy <- dforig$recurrence
@@ -88,6 +110,7 @@ set.seed(2022)
 # examine imbalanced outcome value
 table(dfy)
 
+########################################
 # reduce data dimensionality
 ## Perform PCA 
 pr.out <- prcomp(df , scale = TRUE)
@@ -95,11 +118,9 @@ phi <- pr.out$rotation # save for next predicting dataset so that predictions ar
 Z <- pr.out$x
 
 ## Graph PC1/PC2
-pcs <- data.frame(Z[,1:2])
-pcs <- cbind(pcs, dforig$recurrence)
-ggplot(data = pcs)+
-  geom_point(aes(x=PC1,y=PC2, color=dforig$recurrence), size=2)+
-  theme_bw()
+pcs <- data.frame(cbind(Z[,1], Z[,2]))
+pcs <- cbind(pcs, dfy)
+ggplot(data = pcs)+geom_point(aes(x=X1,y=X2, color=dfy), size=2)+theme_bw()
 
 ## Evaluate PCA Data Variance using elbow method
 PVE.matrix <- summary(pr.out)$importance
@@ -112,33 +133,38 @@ text(x=10, y=0.04)
 dfZ <- data.frame(Z[,1:20]) # find how to do elbow
 dfZ$recurrence <- dfy
 
+############################################
 # test-train split set-up
 index.train <- sample(1:dim(dfZ)[1], 0.8*dim(dfZ)[1])
 data.train <- dfZ[index.train,]
 data.test <- dfZ[-index.train,]
 
-# up and down sampling
+###################################################
+# adjusting dataset to address imbalance
+## up and down sampling
 library("ROSE")
 data.train <- ROSE(recurrence ~ ., data = data.train, seed=2022)$data
 
-# weighting outputs based on proportions
+## weighting outputs based on proportions
 w0 <- length(data.train$recurrence)/(length(data.train$recurrence[data.train$recurrence==0]))
 w1 <- length(data.train$recurrence)/(length(data.train$recurrence[data.train$recurrence==1]))
 ws <- to_vec(for(r in data.train$recurrence) if (r == 0) w0 else w1)
 
-# train model
+############################################
+# train basic model
 logistic.model <- glm(recurrence~., family='binomial', data=data.train, weights=ws)
 
 # basic evaluation of model
 p.predict <- predict(logistic.model, data.test, type='response')
-y.predict <- ifelse(p.predict>=0.65, 1, 0)
+y.predict <- ifelse(p.predict>=0.55, 1, 0)
 y.test <- data.test$recurrence
 table(y.predict, y.test)
 evaluators <- accuracies(prediction = y.predict, truth = y.test)
 evaluators
 
 # cross validate accuracy
-CVAcc <- crossVal(dfZ, 0.65)
+CVAcc <- crossVal(dfZ, 0.55)
+print(CVAcc)
 
 # ROC Curve
 library("PRROC")
@@ -146,21 +172,29 @@ plot(roc.curve(scores.class0 = p.predict[y.test==1],
                scores.class1 = p.predict[y.test==0], curve = TRUE),
      ylab='True Postive Rate', xlab='False Negative Rate (1 - True Negative Rate)')
 
+#############################################
 # backward select using AIC
 step(logistic.model, method='backward')
 
 # train selected model
-selected.model <- glm(formula = recurrence ~ PC2 + PC3 + PC4 + PC5 + PC9 + PC10 + 
-                        PC12 + PC14 + PC16 + PC17 + PC18 + PC19, family = "binomial", 
+selected.model <- glm(formula = recurrence ~ PC3 + PC5 + PC9 + PC10 + 
+                        PC12 + PC14 + PC16, family = "binomial", 
                       data = data.train, weights = ws)
 
 # run evaluation on selected model
 selected.p.predict <- predict(selected.model, data.test, type='response')
-selected.y.predict <- ifelse(selected.p.predict>=0.65, 1, 0) # find a way to select for the best threshold
+selected.y.predict <- ifelse(selected.p.predict>=0.55, 1, 0) # find a way to select for the best threshold
 y.test <- data.test$recurrence
 table(selected.y.predict, y.test)
-selected.acc.test <- mean(selected.y.predict==y.test)
-selected.acc.test
+
+evaluators2 <- accuracies(prediction = selected.y.predict, truth = y.test)
+evaluators2
+
+# cross validate accuracy
+CVAcc <- crossVal(dfZ, 0.55)
+print(CVAcc)
+
+library(PRROC)
 plot(roc.curve(scores.class0 = selected.p.predict[y.test==1], 
                scores.class1 = selected.p.predict[y.test==0], curve = TRUE),
      ylab='True Postive Rate', xlab='False Negative Rate (1 - True Negative Rate)')
