aaa1e7a044c07befb7efa7379b54ce0e80d71522
@rsethi <70125166+rsethi21@users.noreply.github.com>
2023-05-08 14:53:43 -0500

    Update convert.py

--- scripts/convert.py
+++ scripts/convert.py
@@ -1,6 +1,8 @@
+# import necessary packages
 import pandas as pd
 import argparse
 
+# appropriate command line flags to customize functions
 parser = argparse.ArgumentParser(description="convert excel to csv")
 parser.add_argument('-i', '--input', help='input excel file path', required=True)
 parser.add_argument('-o', '--output', help='output csv file path', required=False, default='./output.csv')
@@ -8,6 +10,7 @@ parser.add_argument('-c', '--columnHeader', help='input the start of the actual 
 
 def convert(inputFilePath, outputFilePath, header):
     
+    # some files had multiple header lines and so if user inputs a columnHeader argument greater than 0, then removing all those header rows from the number down to 0
     if header == 0:
         df = pd.read_excel(inputFilePath)
     else:
@@ -15,6 +18,7 @@ def convert(inputFilePath, outputFilePath, header):
 
     df.to_csv(outputFilePath, header=True, index=None)
 
+# run function if ran from command line
 if __name__ == '__main__':
     args = parser.parse_args()
     convert(args.input, args.output, int(args.columnHeader))
